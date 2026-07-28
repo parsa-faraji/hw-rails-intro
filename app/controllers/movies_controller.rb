51d284bb045b3b2e6898f55bdc602cc5afee5917
Parsa Faraji Alamouti <parsafaraji@berkeley.edu>
2026-07-28 02:49:53 +0000

    Remember movie sorting and rating filters

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,18 +2,12 @@ class MoviesController < ApplicationController
   before_action :set_movie, only: %i[show edit update destroy]
 
   SORTABLE_COLUMNS = %w[title release_date].freeze
+# GET /movies or /movies.json
+def index
+  @all_ratings = Movie.all_ratings
 
-  # GET /movies or /movies.json
-  def index
-    @all_ratings = Movie.all_ratings
-
-    @ratings_to_show =
-      if params[:ratings].present?
-        params[:ratings].keys
-      else
-        @all_ratings
-      end
-
+  if sorting_or_filtering_params_present?
+    # The user explicitly submitted new settings.
     @sort_by =
       if SORTABLE_COLUMNS.include?(params[:sort_by])
         params[:sort_by]
@@ -21,10 +15,34 @@ class MoviesController < ApplicationController
         "title"
       end
 
-    @movies = Movie.with_ratings(@ratings_to_show)
-                   .order(@sort_by => :asc)
+    # No ratings parameter means every checkbox was unchecked,
+    # which the assignment says should display every rating.
+    @ratings_to_show =
+      if params[:ratings].present?
+        params[:ratings].keys
+      else
+        @all_ratings
+      end
+
+    # Remember the new settings for later requests.
+    session[:sort_by] = @sort_by
+    session[:ratings] = @ratings_to_show
+  else
+    # No sorting or filtering parameters were supplied, so restore
+    # the previously saved settings.
+    @sort_by =
+      if SORTABLE_COLUMNS.include?(session[:sort_by])
+        session[:sort_by]
+      else
+        "title"
+      end
+
+    @ratings_to_show = session[:ratings].presence || @all_ratings
   end
 
+  @movies = Movie.with_ratings(@ratings_to_show)
+                 .order(@sort_by => :asc)
+end
   # GET /movies/1 or /movies/1.json
   def show
   end
@@ -107,6 +125,10 @@ class MoviesController < ApplicationController
   def set_movie
     @movie = Movie.find(params[:id])
   end
+
+  def sorting_or_filtering_params_present?
+  params.key?(:sort_by) || params.key?(:ratings)
+end 
 
   # Only allow a list of trusted parameters through.
   def movie_params
